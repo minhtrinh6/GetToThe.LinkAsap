c4b748cd56a971eea4cb2ce749bca21b81065ee4
Tri Hoang <trihoang@bu.edu>
2018-07-14 15:00:23 -0400

    Change logging mechanism
    - Send UDP request to another server to process log (saved 50ms response time on Lambda@Edge)

--- log/log.js
+++ log/log.js
@@ -1,36 +1,31 @@
-const AWS = require('aws-sdk');
-var lambda = new AWS.Lambda({region: 'us-east-1'});
-const iotData = new AWS.IotData({endpoint: 'a3g47r8brmmqer.iot.us-east-1.amazonaws.com'});
-var Redis = require('redis');
-var client = Redis.createClient({host: '52.201.47.240', password: 'CuteShibaBouncing'})
+var dgram = require('dgram');
 
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
     const uri = request.uri;
-    
+
     if (uri == '' || uri == '/index.html' || uri == "/" || uri == "/app.jsx") {
         callback(null, request);
         return;
     }
-  
-    lambda.invoke({
-      FunctionName: 'gttlalog-dev-logging',
-      Payload: JSON.stringify(event, null, 2),
-      InvocationType: 'Event'
-    })
     
+    udp_it(request);
+    
+    var link = String(uri).replace("/", "").toLowerCase();
     request['uri'] = "/routes/" + link + ".html"
     callback(null, request)
 }
 
-lambda.invoke({
-  FunctionName: 'name_of_your_lambda_function',
-  Payload: JSON.stringify(event, null, 2) // pass params
-}, function(error, data) {
-  if (error) {
-    context.done('error', error);
-  }
-  if(data.Payload){
-   context.succeed(data.Payload)
-  }
-});
+function udp_it(request) {
+  var PORT = 41234;
+  var HOST = "52.201.47.240"
+
+  var message = new Buffer(JSON.stringify(request))
+  
+  var client = dgram.createSocket('udp4')
+  client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+      if (err) throw err;
+      console.log('UDP message sent to ' + HOST +':'+ PORT);
+      client.close();
+  });
+}
